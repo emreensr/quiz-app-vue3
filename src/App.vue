d860cf75bfe0073f7680e6a6948f9bdbb40d1194
emreensr <emreensar.capci@gmail.com>
2023-04-13 03:52:01 +0300

    quiz app done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import {RouterView} from "vue-router"
+  import QuziesView from "./views/QuziesView.vue";
+
+</script>
+
+<template>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+    max-width: 1000px;
+    margin : 0 auto;
+  }
+
+</style>
